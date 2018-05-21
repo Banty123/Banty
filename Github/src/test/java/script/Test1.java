03e09ce2eada4023be2fc3e33520c4aef69d85f3
Banty <Banty@DESKTOP-7ETDCNK>
2018-05-21 17:10:10 +0530

    commit2

--- Github/src/test/java/script/Test1.java
+++ Github/src/test/java/script/Test1.java
@@ -3,8 +3,8 @@ package script;
 public class Test1 {
 
 	public static void main(String[] args) {
-	System.out.println("This is Maven Project");
-
+//	System.out.println("This is Maven Project");
+		System.out.println("Hello Git");
 	}
 
 }
